f9e7e2d938ab7c7440fe771d39d357f4665a7ac3
tylermachado <3586466+tylermachado@users.noreply.github.com>
2020-01-27 21:53:59 -0500

    work on treemap tooltips

--- src/components/Treemap.js
+++ src/components/Treemap.js
@@ -12,7 +12,12 @@ import { interpolateViridis, schemeCategory10 } from "d3-scale-chromatic"
 class TreeMap extends Component {
   constructor(props){
     super(props)
+    this.state = { x: 0, y: 0 };
     this.createTreeMap = this.createTreeMap.bind(this)
+  }
+
+  _onMouseMove(e) {
+    this.setState({ x: e.screenX, y: e.screenY });
   }
 
   componentDidMount() {
@@ -31,6 +36,11 @@ class TreeMap extends Component {
     const jsondata = this.props.jsondata;
     const colorScale = scaleOrdinal(schemeCategory10); //todo: use viridis
 
+        // Define the div for the tooltip
+    const tooltip =   select(node).append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
     const nestedData = nest()
           .key(function(d) { return d.Agency; })
           .entries(data)
@@ -47,6 +57,7 @@ class TreeMap extends Component {
     var totalSpend = root.value;
 
     colorScale.domain(data);
+
 
       // Then d3.treemap computes the position of each element of the hierarchy
   d3.treemap()
@@ -66,6 +77,19 @@ class TreeMap extends Component {
       .attr('width', function (d) { return d.x1 - d.x0; })
       .attr('height', function (d) { return d.y1 - d.y0; })
       .style("fill", function (d) { return colorScale(d.data.Agency); })
+      .on("mouseover", function(d) {
+            tooltip.transition()
+                .duration(200)
+                .style("opacity", .9);
+            tooltip	.html((d.data.Agency) + "<br/>")
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY - 28) + "px");
+            })
+        .on("mouseout", function(d) {
+            tooltip.transition()
+                .duration(500)
+                .style("opacity", 0);
+        });
 
   // and to add the text labels
   select(node)
@@ -80,8 +104,12 @@ class TreeMap extends Component {
       .text(function(d){
         return ((d.data.FY2017Expenditures > (totalSpend*0.01)) ? d.data.Program : ""); // label only the programs that take up more than 1% of spending
       })
+      // .call(wrap.bounds(
+      //   {height: 600,
+      //     width: function (d) { return 150; }}
+      //   ))
       .attr("font-size", "8px")
-      .attr("fill", "white")
+      .attr("fill", "white");
   }
 
   render() {
