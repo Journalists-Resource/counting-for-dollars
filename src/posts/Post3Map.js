f9d296131a2cffbd37800ad0ac65044ece8caa16
tylermachado <3586466+tylermachado@users.noreply.github.com>
2020-02-12 21:24:24 -0500

    ready undercount data

---
+++ src/posts/Post3Map.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react'
+import '../App.css'
+import { csv } from 'd3-fetch'
+import DataTable from '../components/Table'
+import StateMap from '../components/StateMap'
+import ReactTooltip from 'react-tooltip'
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+
+
+
+
+class Post1Map extends Component {
+  constructor(props){
+    super(props)
+    this.onResize = this.onResize.bind(this)
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      screenWidth: window.innerWidth,
+      screenHeight: 700,
+      hover: "none",
+      data: [],
+      slice: "total",
+      program: "Title I Grants to LEAs"
+    }
+
+  }
+
+  onResize() {
+    this.setState({ screenWidth: window.innerWidth  })
+  }
+
+  // onHover(d) {
+  //   this.setState({ hover: d.id })
+  // }
+
+  componentWillMount() {
+    csv("datasets/fy2016statefunding.csv").then(data => {
+      this.setState({data: data});
+    });
+
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.onResize, false)
+    this.onResize()
+  }
+
+  componentDidUpdate() {
+    ReactTooltip.rebuild()
+  }
+
+  handleClick(e) {
+    this.setState({slice: e})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h2>{this.state.program} Funding</h2>
+        <ButtonGroup id="toggles" aria-label="outlined button group">
+          <Button className="active" onClick={this.handleClick.bind(this, "total")}>Total Funding</Button>
+          <Button className="inactive" onClick={this.handleClick.bind(this, "pop")}>Per Capita</Button>
+          <Button className="inactive" onClick={this.handleClick.bind(this, "income")}>Per Income</Button>
+        </ButtonGroup>
+        <div>
+          <ReactTooltip />
+          <StateMap data={this.state.data} program={this.state.program} size={[this.state.screenWidth, this.state.screenHeight]} slice={this.state.slice}  />
+        </div>
+        <div>
+          <DataTable data={this.state.data}  />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Post1Map
