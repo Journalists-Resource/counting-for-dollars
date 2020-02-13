66f2b8c4e4748af4fd534fe943ff11b7a821e26d
tylermachado <3586466+tylermachado@users.noreply.github.com>
2020-02-12 23:06:36 -0500

    duplicating maps

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ import Post1Tree from './posts/Post1Tree'
 import Post2Tree from './posts/Post2Tree'
 import Post2Map from './posts/Post2Map'
 import Post2Table from './posts/Post2Table'
+import Post3Table from './posts/Post3Table'
 import Post3Map from './posts/Post3Map'
 import Post4Map from './posts/Post4Map'
+import Post4Table from './posts/Post4Table'
+import Post5Table from './posts/Post5Table'
 
 
 class App extends Component {
@@ -24,8 +27,11 @@ class App extends Component {
               <Route path="/Post2Tree" component={Post2Tree}  />
               <Route path="/Post2Map" component={Post2Map}  />
               <Route path="/Post2Table" component={Post2Table}  />
+              <Route path="/Post3Table" component={Post3Table}  />
               <Route path="/Post3Map" component={Post3Map}  />
               <Route path="/Post4Map" component={Post4Map}  />
+              <Route path="/Post4Table" component={Post4Table}  />
+              <Route path="/Post5Table" component={Post5Table}  />
             </Switch>
         </main>
       </HashRouter>
---
+++ src/posts/Post4Table.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react'
+import '../App.css'
+import { csv, json } from 'd3-fetch'
+import DataTable from '../components/Table'
+
+
+
+
+class Post4Table extends Component {
+  constructor(props){
+    super(props)
+    this.onResize = this.onResize.bind(this)
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      screenWidth: window.innerWidth,
+      screenHeight: 700,
+      hover: "none",
+      data: [],
+      slice: "total",
+      program: "Title I Grants to LEAs"
+    }
+
+  }
+
+  onResize() {
+    this.setState({ screenWidth: window.innerWidth  })
+  }
+
+  // onHover(d) {
+  //   this.setState({ hover: d.id })
+  // }
+
+  componentWillMount() {
+    csv("datasets/map-and-table-title-i-grants-per-state-per-child.csv").then(data => {
+      this.setState({data: data});
+    });
+
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.onResize, false)
+    this.onResize()
+  }
+
+  componentDidUpdate() {
+    // ReactTooltip.rebuild()
+  }
+
+  handleClick(e) {
+    this.setState({slice: e})
+  }
+
+  render() {
+    console.log(this.state.data)
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>How the census guides Medicaid and how an undercount can cost states millions in reimbursements</h2>
+        </div>
+        <div>
+          <DataTable data={this.state.data}  />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Post4Table
