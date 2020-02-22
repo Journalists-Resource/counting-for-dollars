4963902d10f8779109081292a3a203f534c50868
tylermachado <3586466+tylermachado@users.noreply.github.com>
2020-02-22 14:23:02 -0500

    prep for Mapbox integration

---
+++ src/posts/Post4DistrictMap.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react'
+import '../App.css'
+import { csv } from 'd3-fetch'
+import DataTable from '../components/Table'
+import StateMap from '../components/StateMap'
+import ReactTooltip from 'react-tooltip'
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import { ChartHeader, ChartFooter } from '../components/ChartMeta'
+
+
+
+
+class Post4Map extends Component {
+  constructor(props){
+    super(props)
+    this.onResize = this.onResize.bind(this)
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      screenWidth: window.innerWidth,
+      screenHeight: 700,
+      hover: "none",
+      data: [],
+      slice: "Funding Per Child",
+      program: "Title I Grants to LEAs"
+    }
+
+  }
+
+  onResize() {
+    this.setState({ screenWidth: window.innerWidth  })
+  }
+
+  // onHover(d) {
+  //   this.setState({ hover: d.id })
+  // }
+
+  componentWillMount() {
+    csv("datasets/map-and-table-title-i-grants-per-state-per-child.csv").then(data => {
+      this.setState({data: data});
+    });
+
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.onResize, false)
+    this.onResize()
+  }
+
+  componentDidUpdate() {
+    ReactTooltip.rebuild()
+  }
+
+  handleClick(e) {
+    this.setState({slice: e})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ChartHeader title="Title I funding per low-income child in 2016 by state" subhed="Wyoming, Vermont and North Dakota received substantially more Title I grant money per eligible child in FY2016 than other states. Each received more than $3,000 per low-income child compared with the national average of $1,489." />
+        <div>
+          <ReactTooltip />
+          <StateMap data={this.state.data} program={this.state.program} size={[this.state.screenWidth, this.state.screenHeight]} slice={this.state.slice} reversescale={true} />
+        </div>
+        <div>
+          <DataTable data={this.state.data}  />
+        </div>
+        <ChartFooter credit="U.S. Census Bureau’s SAIPE; Dept. of Education" />
+      </div>
+    )
+  }
+}
+
+export default Post4Map
