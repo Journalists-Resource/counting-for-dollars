e0231edf466902a316b780b0ffcbe36f4c5924bc
tylermachado <3586466+tylermachado@users.noreply.github.com>
2020-03-01 16:04:42 -0500

    decimal point adjustments

--- src/components/FormatMoney.js
+++ src/components/FormatMoney.js
@@ -1,6 +1,6 @@
 const formatMoney = (value, percentify) => {
    if (percentify === "income") {
-     return (Math.round(value * 10000)/100) + "%"
+     return (Math.round(value * 1000000)/10000) + "%"
    } else if (!isNaN(value)) {
      let newvalue = parseFloat(value);
      return "$" + newvalue.toLocaleString('en-US');
