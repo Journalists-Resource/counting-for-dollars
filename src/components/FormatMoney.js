ce9d99af54c5c45210ee74cb5c929e82a13ea875
tylermachado <3586466+tylermachado@users.noreply.github.com>
2020-03-01 19:03:40 -0500

    fix cents rounding in map

--- src/components/FormatMoney.js
+++ src/components/FormatMoney.js
@@ -1,6 +1,12 @@
 const formatMoney = (value, percentify) => {
    if (percentify === "income") {
      return (Math.round(value * 1000000)/10000) + "%"
+   } else if (!isNaN(value) && value<10000) {
+     let newvalue = parseFloat(value);
+     return (Math.round(newvalue*1000)/1000).toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      });
    } else if (!isNaN(value)) {
      let newvalue = parseFloat(value);
      return "$" + newvalue.toLocaleString('en-US');
