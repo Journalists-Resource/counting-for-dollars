edf93018b40a1b792488d4f4ebde02a9d6a7fee7
tylermachado <3586466+tylermachado@users.noreply.github.com>
2020-03-17 19:44:59 -0400

    exclude cents in deeper negative values

--- src/components/FormatMoney.js
+++ src/components/FormatMoney.js
@@ -1,7 +1,7 @@
 const formatMoney = (value, percentify) => {
    if (percentify === "income") {
      return (Math.round(value * 1000000)/10000) + "%"
-   } else if (!isNaN(value) && value<10000) {
+   } else if (!isNaN(value) && value<10000 && value>-10000) {
      let newvalue = parseFloat(value);
      return (Math.round(newvalue*1000)/1000).toLocaleString('en-US', {
         style: 'currency',
